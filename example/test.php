98c081a52a31de8f9e751209b987ebf9c17fd7d0
evilk <540621396@qq.com>
2020-07-08 15:48:54 +0800

    fix

--- example/test.php
+++ example/test.php
@@ -3,7 +3,9 @@
 require dirname(__DIR__) . "/vendor/autoload.php";
 
 use Tool\EchoHello;
+use Tool\aa\aaTest;
 
 EchoHello::echoHello();
 
 
+aaTest::aaTest();
---
+++ src/tool/aa/aaTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Tool\aa;
+
+class aaTest
+{
+    public static function aaTest()
+    {
+        echo 'aaTest' . PHP_EOL;
+    }
+}
